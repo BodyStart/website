fd2c49b41ce18135dba581304e0725e1c7ccee5e
Alexandre Canacaris <tiloucanac1300@gmail.com>
2024-02-10 08:52:30 +0100

    css du header et responsive du header + resposive tel

--- js/styles.js
+++ js/styles.js
@@ -1,1 +1,15 @@
+document.addEventListener('DOMContentLoaded', function() {
+  const hamburger = document.querySelector('.hamburger');
+  const nav23 = document.querySelector('.nav23');
+  const closeBtn = document.querySelector('.close-btn');
 
+  hamburger.addEventListener('click', function() {
+      nav23.classList.toggle('nav23-active');
+      this.classList.toggle('active'); // Toggle active class for hamburger
+  });
+
+  closeBtn.addEventListener('click', function() {
+      nav23.classList.remove('nav23-active');
+      hamburger.classList.remove('active'); // Remove active class when nav is closed
+  });
+});
