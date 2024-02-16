3698a33f8c849fe7f8aebf1dfec57689c6faf3a6
Hassan <derkaouihassan19@gmail.com>
2024-02-16 14:17:41 +0100

    create component basic

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
 import './App.css'
 import Header from "./components/Header";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 
 function App() {
 
-  return (
-    <>
-      <Header />
-    </>
-  )
+    return (
+        <BrowserRouter>
+            <Header/>
+            <Routes>
+                <Route path="/" element={<Layout/>}>
+                    <Route index element={<Home/>}/>
+                    <Route path="blogs" element={<Blogs/>}/>
+                    <Route path="contact" element={<Contact/>}/>
+                    <Route path="*" element={<NoPage/>}/>
+                </Route>
+            </Routes>
+        </BrowserRouter>
+    )
 }
 
 export default App
---
+++ src/components/Footer.jsx
@@ -0,0 +1,20 @@
+function Header() {
+    return (
+        <footer className="footer">
+            <hr className="footer-hr" />
+            <div className="footer-content">
+                <p>BODYSTART</p>
+                <div className="wrapper">
+                    <ul className="icon-list">
+                        <li className="item"><a href="https://www.instagram.com/adam_.lc/"><i className="fa-brands fa-instagram icon"></i></a></li>
+                        <li className="item"><a href="tel:0761847580"><i className="fa-brands fa-whatsapp icon"></i></a></li>
+                        <li className="item"><a href="mailto:bodystartpro@gmail.com"><i className="fas fa-envelope icon"></i></a></li>
+                        <li className="item"><a href="https://discord.gg/rhGMPCQM"><i className="fa-brands fa-discord icon"></i></a></li>
+                    </ul>
+                </div>
+                <p>@2024 Bodystart</p>
+            </div>
+        </footer>
+    );
+}
+export default Header;
