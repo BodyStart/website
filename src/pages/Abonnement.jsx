d13c31e04282f9a0d4a48d0ddbe8a5884d2cef2e
Hassan <derkaouihassan19@gmail.com>
2024-02-17 14:28:49 +0100

    page subscription & program done

--- src/pages/Abonnement.jsx
+++ src/pages/Abonnement.jsx
@@ -1,9 +1,57 @@
+import '../assets/css/abonnement.css'
+
 function Abonnement() {
     return (
-        <>
-            <h1>Abonnement</h1>
-            <p>Welcome to the Abonnement page.</p>
-        </>
+        <main>
+            <div className="placement">
+                <div className="pricing-card">
+                    <h2>Entraînement</h2>
+                    <h5>Programme sport</h5>
+                    <span className="price">€40.00</span>
+                    <h5>4 semaines</h5>
+                    <h3>Contenu</h3>
+                    <ul className="list-group">
+                        <li className="truc">- Programme d'entraînement personnalisé</li>
+                        <li className="truc">- Conseils exercices</li>
+                        <li className="truc">- Bilan toutes les 2 semaines</li>
+                        <li className="truc">- Ajustement selon le bilan</li>
+                        <li className="truc">- Echange via whatapps</li>
+                    </ul>
+                    <button className="btn-primary">En savoir plus</button>
+                </div>
+                <div className="pricing-card">
+                    <span className="label-best">Best deal</span>
+                    <h2>Complet</h2>
+                    <h5>Programme sport + plan alimentaire</h5>
+                    <span className="price">€60.00</span>
+                    <h5>4 semaines</h5>
+                    <h3>Contenu</h3>
+                    <ul className="list-group">
+                        <li className="truc">- Programme d'entraînement personnalisé</li>
+                        <li className="truc">- Plan alimentaire personnalisé</li>
+                        <li className="truc">- Bilan toutes les 2 semaines</li>
+                        <li className="truc">- Ajustement selon le bilan</li>
+                        <li className="truc">- Echange via whatapps</li>
+                    </ul>
+                    <button className="btn-primary">En savoir plus</button>
+                </div>
+                <div className="pricing-card">
+                    <h2>Nutrition</h2>
+                    <h5>Plan alimentaire</h5>
+                    <span className="price">€30.00</span>
+                    <h5>4 semaines</h5>
+                    <h3>Contenu</h3>
+                    <ul className="list-group">
+                        <li className="truc">- Plan alimentaire personnalisé</li>
+                        <li className="truc">- Conseils nutrition</li>
+                        <li className="truc">- Bilan toutes les 2 semaines</li>
+                        <li className="truc">- Ajustement selon le bilan</li>
+                        <li className="truc">- Echange via whatapps</li>
+                    </ul>
+                    <button className="btn-primary">En savoir plus</button>
+                </div>
+            </div>
+        </main>
     );
 }
 export default Abonnement;
--- src/pages/Programme.jsx
+++ src/pages/Programme.jsx
@@ -1,9 +1,63 @@
+import '../assets/css/programme.css'
+
 function Programme() {
     return (
-        <>
-            <h1>Programme</h1>
-            <p>Welcome to the Programme page.</p>
-        </>
+        <main>
+            <div className="placement">
+                <div className="programme">
+                    <img
+                        src="/src/assets/images/Débutant.png"
+                        alt="programme1"
+                        className="programme-image"
+                    />
+                    <h2>Programme de musculation Débutant</h2>
+                    <span className="price">€25.00</span>
+
+                    <a href="/Programme/3.1.debutant.html">
+                        <button className="btn-primary">En savoir plus</button>
+                    </a>
+                </div>
+                <div className="programme">
+                    <img
+                        src="/src/assets//images/Avancée.png"
+                        alt="programme1"
+                        className="programme-image"
+                    />
+                    <h2>Programme de musculation Avancé</h2>
+                    <span className="price">€30.00</span>
+
+                    <a href="/Programme/3.2.avance.html">
+                        <button className="btn-primary">En savoir plus</button>
+                    </a>
+                </div>
+                <div className="programme">
+                    <img
+                        src="/src/assets//images/Bodybuilder.png"
+                        alt="programme1"
+                        className="programme-image"
+                    />
+                    <h2>Programme de musculation Bodybuilder</h2>
+                    <span className="price">€35.00</span>
+
+                    <a href="/Programme/3.3.bodybuilder.html">
+                        <button className="btn-primary">En savoir plus</button>
+                    </a>
+                </div>
+                <div className="programme">
+                    <img
+                        src="/src/assets//images/Personnalisé.png"
+                        alt="programme1"
+                        className="programme-image"
+                    />
+                    <h2>Programme de musculation Personnalisé</h2>
+                    <span class="price">€50.00</span>
+
+                    <a href="/Programme/3.4.personnalise.html">
+                        <button class="btn-primary">En savoir plus</button>
+                    </a>
+                </div>
+            </div>
+        </main>
     );
 }
 export default Programme;
